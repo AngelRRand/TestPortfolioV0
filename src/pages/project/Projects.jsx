ac9ce5230e60832610c4d3305d7d770b65c361da
AngelRRand <horacio27rodriguez_cba@hotmail.com>
2022-08-08 18:08:07 -0300

    StylesProducts

--- src/pages/project/Projects.jsx
+++ src/pages/project/Projects.jsx
@@ -1,6 +1,28 @@
 import React from 'react'
 
 const Projects = () => {
+
+  const ProjectsContainer = styled.div`
+    overflow: hidden;
+    width: 100vw;
+    height: 100vh;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    `
+  const PlanetContainer = styled.div`
+    display: flex;
+    width: 450px;
+    height: 450px;
+    border: none;
+    box-shadow: none;
+    overflow: hidden;
+    img{
+      width: 100vw;
+      image-rendering: pixelated;
+    }
+    z-index: 1;
+  `
   return (
     <div>
         <p>
