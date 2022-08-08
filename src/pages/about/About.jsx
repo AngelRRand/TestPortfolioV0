a177eeea280e1a871e0e8375112b022efafd7fb2
AngelRRand <horacio27rodriguez_cba@hotmail.com>
2022-08-08 20:01:25 -0300

    fixed

--- src/pages/about/About.jsx
+++ src/pages/about/About.jsx
@@ -7,16 +7,16 @@ const About = () => {
 
 
   const AboutContainer = styled.div`
-    overflow: hidden;
-    width: 100vw;
     display: flex;
+    width: 100vw;
+    height: 100vh;
     justify-content: center;
     align-items: center;
     `
   const ContainerIMG = styled.div`
     position: absolute;
-    width: 40vw;
-    height: 40vh;
+    width: 30vw;
+    height: 30vh;
     object-fit: cover;
     img{
       width:100%;
@@ -24,13 +24,13 @@ const About = () => {
     }
   `
   const TempladeGrid = styled.div`
-  display: grid;
-  width: 40vw;
-  height:  45vh;
-  grid-template-columns: repeat(10, 4vw);
-  grid-template-rows: repeat(10, 6vh);
-  grid-column-gap: 0px;
-  grid-row-gap: 0px;
+    display: grid;
+    width: 40vw;
+    height:  40vh;
+    grid-template-columns: repeat(20, 2vw);
+    grid-template-rows: repeat(20, 2vh);
+    grid-column-gap: 0px;
+    grid-row-gap: 0px;
   `
 
   return (
