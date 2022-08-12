6b3b8b90e92a6bdc11cf702ed74b92cc3c3dfe48
AngelRRand <horacio27rodriguez_cba@hotmail.com>
2022-08-12 01:13:06 -0300

    refreshIMG

--- src/component/About/House.jsx
+++ src/component/About/House.jsx
@@ -1,6 +1,6 @@
 import React, { useState } from 'react'
 import styled from 'styled-components';
-import house from '../../img/casa.gif'
+import house from '../../img/House2.png'
 
 
 const House = () => {
