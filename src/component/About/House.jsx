9920657623b4472354630faba67d288fba67ea05
AngelRRand <horacio27rodriguez_cba@hotmail.com>
2022-08-19 18:05:00 -0300

    costo

--- src/component/About/House.jsx
+++ src/component/About/House.jsx
@@ -1,13 +1,18 @@
 import React, { useState } from 'react'
 import styled from 'styled-components';
 import house from '../../img/160x90v2.png'
-
+import { Link } from 'react-router-dom';
+import { motion } from "framer-motion";
 
 const House = () => {
-  
 
+  const House = styled.div`
+    overflow: hidden;
+    width: 100vw;
+    height: 85vh;
+  `
   const ContainerHouse = styled.div`
-    display: flex;
+    position: absolute;
     align-items: center;
     justify-content: center;
     image-rendering: pixelated;
@@ -18,10 +23,31 @@ const House = () => {
     image-rendering: pixelated;
     z-index: 10;
   `
+  const Nubes = styled.div`
+  width: 150vw;
+  height: 85vh;
+  background-color: white;
+  z-index: 50;
+  position: absolute;
+`
   return (
-    <ContainerHouse>
-      <BackGround src={house} alt="" />
-    </ContainerHouse>
+    <>
+      <House>
+
+        <ContainerHouse>
+          <BackGround src={house} alt="" />
+        </ContainerHouse>
+        <motion.div
+          initial={{ x: -500, y: -0 }}
+          animate={{
+            x: 5000,
+            transition: { duration: 3.5, type: "tween" },
+          }}
+        >
+          <Nubes />
+        </motion.div>
+      </House>
+    </>
   )
 }
 
--- src/component/About/HouseTransition.jsx
+++ src/component/About/HouseTransition.jsx
@@ -1,4 +1,4 @@
-import React, {useState} from 'react'
+import React, { useState } from 'react'
 import styled from 'styled-components';
 import { Link } from 'react-router-dom';
 import { motion } from "framer-motion";
@@ -8,7 +8,7 @@ const House = () => {
   const navigate = useNavigate()
   const go = () => {
     navigate('/About/House')
-}
+  }
   const AboutContainer = styled.div`
     overflow: hidden;
     height: 85vh;
@@ -34,7 +34,6 @@ const House = () => {
     font-family: 'PF Tempesta Seven Condensed', sans-serif;
     font-family: 'PF Tempesta Seven Compressed', sans-serif;
     img{
-      position: absolute;
       width: 100%;
       height: 100%;
       image-rendering: pixelated;
@@ -47,43 +46,40 @@ const House = () => {
     height: 85vh;
     background-color: white;
     z-index: 100;
-    top: 0;
-    right: 0;
   `
-  /* setTimeout(() => {
+  setTimeout(() => {
     go()
-  }, 5000); */
+  }, 5000);
   return (
     <>
-
-    <AboutContainer>
-    <motion.div
-        initial={{ scale: 1,  }}
-        animate={{
-          scale: 5,
-          x: 200,
-          y: -850,
-          transition: { duration: 6.5, type: "spring", },
-        }}
-      >
-        
-        <ContainerIMG>
-          <h3>About</h3>
-          <img src={planet} alt="" />
-        </ContainerIMG>
-        
-      </motion.div>
-      <motion.div
-        initial={{ x: 10000 }}
+      
+      {/* <motion.div
+        initial={{ x: 10000, }}
         animate={{
           x: 0,
-          transition: { duration: 6.5, type: "tween", when: "afterChildren"},
+          transition: { duration: 6.5, type: "tween" },
         }}
       >
-        <Nubes/>
-      </motion.div>
-       
-    </AboutContainer>
+        <Nubes />
+      </motion.div> */}
+      <AboutContainer>
+        <motion.div
+          initial={{ scale: 1, }}
+          animate={{
+            scale: 5,
+            x: 200,
+            y: -850,
+            transition: { duration: 6.5, type: "spring",bounce: 0 },
+          }}
+        >
+
+          <ContainerIMG>
+            <h3>About</h3>
+            <img src={planet} alt="" />
+          </ContainerIMG>
+
+        </motion.div>
+      </AboutContainer>
     </>
   )
 }
