dc86c583b9eeadcb7cb8f4b38b7f6999492390c3
AngelRRand <horacio27rodriguez_cba@hotmail.com>
2022-09-10 19:57:28 -0300

    motions

--- src/component/About/Dream.jsx
+++ src/component/About/Dream.jsx
@@ -19,7 +19,35 @@ const Dream = () => {
                 bg={background}
                 bgGif={backgroundGif}
             />
+            <motion.div
+                initial={{ opacity: 0 }}
+                animate={{
+                    opacity: 1,
+                    transition: { duration: 4.5, type: "tween" },
+                }}
+            >
+                {/* <TemplateGrid>
+          <Me>
+            <img src={lightAuto} alt='light' />
+            <img src={lightHover} alt='light' />
+            <img src={lightEffect} alt='light' />
+            <Cart>
+              <h2>Room</h2>
+              <p>In front of a window I find myself in constant contact with the sunlight.</p>
+            </Cart>
+          </Me>
+          <Cat>
 
+            <img src={catBody} alt='cat' />
+            <img src={cathead} alt='cat' />
+            <img src={catheadHover} alt='cat' />
+            <Cart>
+              <h2>My cat</h2>
+              <p>His name is twenty-seven, he is always at my house :D</p>
+            </Cart>
+          </Cat>
+        </TemplateGrid> */}
+            </motion.div>
             <TransitionTerra />
         </Container>
     )
