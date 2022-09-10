45efdb6298b2f36ec3a7167f8decff9bfaf96c6e
AngelRRand <horacio27rodriguez_cba@hotmail.com>
2022-09-10 19:56:10 -0300

    transitionsDream

--- src/component/About/Dream.jsx
+++ src/component/About/Dream.jsx
@@ -1,9 +1,28 @@
-import React from 'react'
-
+import React, { useState } from 'react'
+import styled from 'styled-components';
+import { motion } from "framer-motion";
+import background from '../../img/Planet/Terra/Dream/Dream.png'
+import backgroundGif from '../../img/Planet/Terra/Dream/Dream.png'
+import TransitionTerra from '../Nav/TransitionTerra';
+import Background from '../Nav/Background';
 const Dream = () => {
-  return (
-    <div>Dream</div>
-  )
+
+    const Container = styled.div`
+    overflow:hidden;
+    width: 100vw;
+    height: 85vh;
+  `
+
+    return (
+        <Container>
+            <Background
+                bg={background}
+                bgGif={backgroundGif}
+            />
+
+            <TransitionTerra />
+        </Container>
+    )
 }
 
 export default Dream
